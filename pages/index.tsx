1627f57f92334c9d2437836578367f87f27943cd
Simon <simonjudd2@gmail.com>
2023-05-04 18:13:19 -0600

    new color scheme and faster loading

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,41 @@
-import Head from 'next/head'
-import Link from "next/link";
+import Head from 'next/head';
+import Link from 'next/link';
 import Typewriter from 'typewriter-effect';
 
 export default function Home() {
   return (
-    <div class="h-screen bg-dark-cyan">
-        <div class="flex justify-center">
-          <div class="absolute md:top-64 top-64">
-            <h1 class="text-center font-neue text-6xl md:text-9xl text-magentaVibrant">BigSky</h1>
+    <>
+      <style jsx>{`
+        .bg-dark-cyan {
+          background-size: cover;
+          background-position: center;
+          background-repeat: no-repeat;
+        }
+      `}</style>
+      <div className="h-screen bg-dark-cyan">
+        <div className="flex justify-center">
+          <div className="absolute md:top-64 top-64">
+            <h1 className="text-center font-neue text-6xl md:text-9xl text-magentaVibrant">BigSky</h1>
             <br></br>
             <br></br>
-            <h1 class="text-center font-neue text-6xl md:text-9xl text-magentaVibrant">🤖</h1>
-              <div class="font-italics pt-8 text-2xl md:text-6xl text-goldVibrant">
-               <Typewriter options={{skipAddStyles: true}} onInit={(typewriter) => {
-                  typewriter.typeString('we fucking love crypto')
-                  .callFunction(() => {
-                    console.log('String typed out!');
-                  })
-                  .pauseFor(2500)
-                  .start();
-               }}
-            />
-           </div>
+            <h1 className="text-center font-neue text-6xl md:text-9xl text-magentaVibrant">🤖</h1>
+            <div className="font-italics pt-8 text-2xl md:text-6xl text-goldVibrant">
+              <Typewriter
+                options={{ skipAddStyles: true, cursor: '' }}
+                onInit={(typewriter) => {
+                  typewriter
+                    .typeString('we fucking love crypto')
+                    .callFunction(() => {
+                      console.log('String typed out!');
+                    })
+                    .pauseFor(100)
+                    .start();
+                }}
+              />
+            </div>
           </div>
         </div>
-    </div>
-  )
+      </div>
+    </>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite-react/**/*.js",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      animation: {
+           'pulse-slow': 'customPulse 3.5s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
+      colors: {
+        'dark-cyan': '#141e24',
+        'burned-gold': '#CD955C',
+        'indian-red': '#CD5C5C',
+        'purple900': '#1C0337',
+        'soft-white': '#faf9fa',
+        'goldVibrant': '#FEB239',
+        'magentaVibrant': '#FC72FF',
+        'redVibrant' : '#F14544',
+        'goldVibrant': '#FEB239',
+        'skyBlue': '#87CEEB',
+      },
+      fontFamily: {
+        neue: ("NeueMachine"),
+        italics: ("NeueMachina-PlainRegularItalic"),
+      },
+      keyframes: {
+        customPulse: {
+          '0%, 100%': {
+            opacity: '1'
+          },
+          '50%': {
+            opacity: '.64'
+          },
+        },
       },
     },
   },
-  plugins: [],
-}
+  plugins: [
+      require("flowbite/plugin")
+  ],
+};
